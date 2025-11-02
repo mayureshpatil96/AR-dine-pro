d054faf1efe1294ec5065e7b9350ff3428a81c76
Mayuresh Patil <mayureshpatil2k4@gmail.com>
2025-11-02 22:16:55 +0530

    Delete src directory

--- src/pages/ARView.jsx
+++
@@ -1,4 +0,0 @@
-export default function ARView() {
-    return <div className="p-4 text-xl">AR Dish View Coming Soon 🥽</div>;
-  }
-  
--- src/pages/Admin.jsx
+++
@@ -1,4 +0,0 @@
-export default function Admin() {
-    return <div className="p-4 text-xl">Admin Dashboard</div>;
-  }
-  
